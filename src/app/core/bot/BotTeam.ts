044241b66370a75e6c8bfba7284e878128ad8d90
shane walsh <shaneneilwalsh@gmail.com>
2022-05-30 21:21:21 +0100

    Working on teams logic

--- src/app/core/bot/BotTeam.ts
+++ src/app/core/bot/BotTeam.ts
@@ -1,13 +1,64 @@
 
 enum TeamRelationship {
   ALLIED="ALLIED",
-  NEUTRAL="NEUTRAL",
-  WAR="WAR"
+  NEUTRAL="NEUTRAL", // default if no relationship is estabilished.
+  ENEMIES="ENEMIES"
 }
 /**
  * A bot team tells which team this bot is associated with.
- * Teams can be allied with other teams, Neutral or at war.
+ * Teams can be allied with other teams, Neutral or at ENEMIES.
  */
-export interface BotTeam {
+export class BotTeam {
+
+  constructor(public name:String) {
+
+  }
 
 }
+
+export class TeamHandler {
+  private relationships:Map<TeamRelationship,RelationshipKey[]> = new Map();
+
+  addRelationship(teamA:BotTeam, teamB:BotTeam, relationship:TeamRelationship){
+    if(!this.relationships.has(relationship)){
+      this.relationships.set(relationship,[]);
+    }
+    this.relationships.get(relationship).push(new RelationshipKey(teamA,teamB));
+  }
+
+  getRelationship(teamA:BotTeam, teamB:BotTeam):TeamRelationship {
+    for(let relationship of this.relationships.keys()){
+      let values = this.relationships.get(relationship);
+      for(let key of values){
+        if(key.isKey(teamA,teamB))
+        return relationship;
+      }
+    }
+    return TeamRelationship.NEUTRAL;
+  }
+
+  removeRelationship(teamA:BotTeam, teamB:BotTeam){
+    for(let relationship of this.relationships.keys()){
+      let values = this.relationships.get(relationship);
+      for(let key of values){
+        if(key.isKey(teamA,teamB)) {
+          this.relationships.set(relationship,values.filter(k => k !== key));
+          // why not break the loop? because the relationship might be mapped more than once, so lets clean house, remove all traces.
+        }
+      }
+    }
+  }
+
+  addOrUpdateRelationship(teamA:BotTeam, teamB:BotTeam, addRelationship:TeamRelationship){
+    this.removeRelationship(teamA,teamB);
+    this.addRelationship(teamA,teamB,addRelationship);
+  }
+}
+
+class RelationshipKey {
+  constructor(public teamA:BotTeam, public teamB:BotTeam){}
+  isKey(teamA:BotTeam, teamB:BotTeam) {
+    return (teamA === this.teamA || teamA == this.teamB) && (teamB === this.teamA || teamB == this.teamB);
+  }
+}
+
